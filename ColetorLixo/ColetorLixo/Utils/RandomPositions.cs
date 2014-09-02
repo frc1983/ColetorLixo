8a550c7224e999f94236f13ce9982b4bf9dfa769
Fabio Costa <fabiorochadacosta@gmail.com>
2014-09-02 20:26:54 -0300

    Adicao de classe de posicao random

---
+++ ColetorLixo/ColetorLixo/Utils/RandomPositions.cs
@@ -0,0 +1,23 @@
+﻿using ColetorLixo.Models;
+using ColetorLixo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ColetorLixo.Utils
+{
+    public class RandomPositions : Controller
+    {
+        public static int GetNextX(Cell[,] ambient)
+        {
+            return new Random().Next(0, ambient.GetLength(0));
+        }
+
+        public static int GetNextY(Cell[,] ambient)
+        {
+            return new Random().Next(0, ambient.GetLength(1));
+        }
+    }
+}
